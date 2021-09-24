86b0778a5aef425c73ae6a7360f7b2f495d2d029
Frank Schulz <Frank-Schulz@live.nmit.ac.nz>
2021-09-24 22:57:31 +1200

    products and users data is now generated using faker

--- YourProject/server/data/products.data.js
+++ YourProject/server/data/products.data.js
@@ -1,16 +1,31 @@
-const productsData = [
-    {
-        name: "ProductOne",
-        price: 100.00,
-        cartDesc: "Lorem ipsum dolor sit amet, consectetur adip",
-        shortDesc: "Lorem ipsum dolor sit amet",
-        longDesc: "Lorem ipsum dolor sit amet",
-        thumbPath: "../images/products/thumb/productOne",
-        imagePath: "../images/products/productOne",
-        stock: 2314,
-        live: true,
-        unlimited: false,
-    }
+const faker = require('faker');
+
+let productsData = [
+    // {
+    //     name: "ProductOne",
+    //     price: 100.00,
+    //     // cartDesc: "Lorem ipsum dolor sit amet, consectetur adip",
+    //     // shortDesc: "Lorem ipsum dolor sit amet",
+    //     description: "Lorem ipsum dolor sit amet",
+    //     // thumbPath: "../images/products/thumb/productOne",
+    //     imagePath: "../images/products/productOne",
+    //     stock: 2314,
+    //     live: true,
+    //     unlimited: false,
+    // }
 ];
+
+for (var i = 0; i < 100; i++) {
+    productsData.push({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        description: faker.commerce.productDescription(),
+        imagePath: faker.image.imageUrl(),
+        stock: faker.datatype.number(1000),
+        live: faker.datatype.boolean(),
+        unlimited: faker.datatype.boolean(),
+    });
+};
 
 module.exports = productsData;
--- YourProject/server/data/users.data.js
+++ YourProject/server/data/users.data.js
@@ -1,10 +1,13 @@
-const usersData = [
+const faker = require('faker');
+
+let usersData = [
     {
         email: "admin@example.com",
         password: "Password123",
         isAdmin: true,
         firstName: "Frank",
         lastName: "Admin",
+        DoB: "11/09/1999",
         addresses: [ {
             city: "Nelson",
             state: "Nelson",
@@ -12,8 +15,27 @@ const usersData = [
             country: "New Zealand",
             address: "Nikau Apartments",
             address2: "85 Nile Street"
-        } ]
+        } ],
     },
 ];
+
+for (let i = 0; i < 10; i++) {
+    usersData.push({
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        isAdmin: faker.datatype.boolean(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        DoB: faker.date.past(),
+        addresses: [ {
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zip: faker.address.zipCode(),
+            country: faker.address.country(),
+            address: faker.address.streetName(),
+            address2: faker.address.streetAddress(),
+        } ],
+    });
+};
 
 module.exports = usersData;
